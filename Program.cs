b9fab4605711d9671f6e27afeeffb68769e55ddc
Wallace Tácio <wallacett.dev@gmail.com>
2021-11-02 23:19:24 -0300

    Adiciona resolução ao exercício Maior 2 Números ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Maior2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n1, n2, mn;
+
+            Console.Write("Digite o primeiro número:..");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o segundo número:...");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+//Primeira Resolução
+            /*if(n1 > n2){
+                mn = n1;
+            }
+            else
+            {
+                mn = n2;
+            }
+            */
+//Segunda Resolução        
+           /* mn = n2;
+
+            if(n1 > n2){
+                mn = n1;
+            }*/
+// terceira resolução
+
+            mn = n1 > n2 ? n1 : n2;
+
+            Console.WriteLine($"O maior número é: {mn}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Maior2Numeros
-Exercício Maior 2 Números 
+## Exercício `Maior2Numeros` ETEC 2021
+
+Receba dois números. Exiba o maior.
+
+Ex.:
+```
+Digite o primeiro número..: 7
+Digite o segundo número...: 15
+O maior número é 15
+```
